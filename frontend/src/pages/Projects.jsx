959c6855c0aa6539264266bb733360276a9d0ce5
Bruno De Paoli <bruno.paoli@evo.ag>
2024-07-02 09:57:21 -0300

    Fazendo ajustes na pagina de projettos

--- frontend/src/pages/Projects.jsx
+++ frontend/src/pages/Projects.jsx
@@ -10,14 +10,17 @@ const Projects = () => {
 
     // Verifica se há token JWT
     if (!token) {
+        console.log('User', user);
         // Redireciona para a página de login se o usuário não estiver autenticado
         console.log('Usuário deslogado');
         return (<Navigate to={'/login'} />);
     } else {
         console.log('Usuário logado');
+        const userJson = JSON.parse(user);
+
         return (
             <div className="w-full min-h-screen flex items-center justify-center flex-col bg-[#2e2157] p-5">
-                <h1 className="md:text-3xl font-black text-left text-[#5fdaa4]">Os Projetos de {user?.name}</h1>
+                <h1 className="md:text-3xl font-black text-left text-[#5fdaa4]">Os Projetos de {userJson.name}</h1>
                 <ProjectList />
             </div>
         );
